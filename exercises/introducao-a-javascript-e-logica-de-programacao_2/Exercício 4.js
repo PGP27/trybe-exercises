709c1e7f08edc704d23a6b6823b07f509bef75c4
Pedro Gonçalves <peg.gep27@gmail.com>
2021-04-23 15:01:45 -0300

    Exercicios 1, 2, 3 e 4

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Exercício 4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i of numbers) {
+    soma+=i;
+}
+
+if (soma/numbers.length > 20) {
+    console.log("valor maior que 20");
+}
+else {
+    console.log("valor menos ou igual a 20");
+}
