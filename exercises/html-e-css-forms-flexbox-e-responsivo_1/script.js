06813bec66c0f842c6f9271f4da4513806ad9f37
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-11 15:00:41 -0300

    Exercicio 1

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    let estados = ["Acre",
+        "Alagoas",
+        "Amapá",
+        "Amazonas",
+        "Bahia",
+        "Ceará",
+        "Espírito Santo",
+        "Goiás",
+        "Maranhão",
+        "Mato Grosso",
+        "Mato Grosso do Sul",
+        "Minas Gerais",
+        "Pará",
+        "Paraíba",
+        "Paraná",
+        "Pernambuco",
+        "Piauí",
+        "Rio de Janeiro",
+        "Rio Grande do Norte",
+        "Rio Grande do Sul",
+        "Rondônia",
+        "Roraima",
+        "Santa Catarina",
+        "São Paulo",
+        "Sergipe",
+        "Tocantins",
+        "Distrito Federal"];
+
+        let select = document.getElementById("input-estado");
+
+        for(let i = 0; i < estados.length; i++) {
+            let option = document.createElement("option");
+            option.innerHTML = estados[i];
+            select.appendChild(option);
+        }
+}
