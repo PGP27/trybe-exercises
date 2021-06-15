8018c2f59d73b9ee2fea96fb87cd5b1d238761b4
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-15 18:06:07 -0300

    exercise 3, 4 e 5

--- exercises/testes-automatizados-com-jest_2/exercises.test.js
+++ exercises/testes-automatizados-com-jest_2/exercises.test.js
@@ -43,3 +43,38 @@ describe('Exercise 2 e 3: ', () => {
     }
   });
 });
+
+const fetch = require('node-fetch');
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name);
+    });
+};
+
+describe('Exercise 4: ', () => {
+  it('Verify find "sd-01-week4-5-project-todo-list" and "sd-01-week4-5-project-meme-generator" with getRepos function', async () => {
+    const array = await getRepos('https://api.github.com/orgs/tryber/repos');
+    const arrayFind = [];
+    array.forEach((arr) => {
+      if(arr === 'sd-01-week4-5-project-todo-list' || arr === 'sd-01-week4-5-project-meme-generator' ) {
+        arrayFind.push(arr);
+      }
+    });
+    expect(arrayFind).toEqual(['sd-01-week4-5-project-meme-generator', 'sd-01-week4-5-project-todo-list']);
+  });
+});
+
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+
+test('', () => console.log('1 - test'));
+
+describe('Scoped / Nested block', () => {
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('', () => console.log('2 - test'));
+});
