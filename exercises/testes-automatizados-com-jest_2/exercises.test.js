5da38e1fd7acbc775facd32830e6bcd85b8d9c8c
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-15 17:38:59 -0300

    exercise 2

--- exercises/testes-automatizados-com-jest_2/exercises.test.js
+++ exercises/testes-automatizados-com-jest_2/exercises.test.js
@@ -7,6 +7,39 @@ describe('Exercise 1: ', () => {
     uppercase('ola mundo', (result) => {
       expect(result).toBe('OLA MUNDO');
       done();
-    })
+    });
+  });
+});
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' },
+};
+  
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      };
+  
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+  
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+
+describe('Exercise 2 e 3: ', () => {
+  it('Verify find user with getUserName function', async () => {
+    const user = await getUserName(4);
+    expect(user).toBe('Mark');
+  });
+  it('Verify not find user with getUserName function', async () => {
+    try{
+      await getUserName(1);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with ' + 1 + ' not found.' });
+    }
   });
 });
